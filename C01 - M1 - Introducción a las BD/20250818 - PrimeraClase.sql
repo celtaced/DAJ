651861212aa9a98638f017f9c894b06e35fbf136
Carlos Dubón <celtaced@gmail.com>
2025-08-19 19:47:54 -0600

    Clase03

---
+++ C01 - M1 - Introducción a las BD/20250818 - PrimeraClase.sql
@@ -0,0 +1,15 @@
+-- Crear una base de datos
+-- create database [Nombre la base de datos]
+
+CREATE DATABASE Prueba01
+
+-- Errores
+Create databse Prueba02
+
+-- ERROR:  error de sintaxis en o cerca de «databse»
+-- LINE 1: Create databse Prueba02
+--                ^
+-- SQL state: 42601
+-- Character: 8
+
+create database Prueba02;
