5002c3c804c4a094a309ea20e5350b596cf433e4
Carlos Dubón <celtaced@gmail.com>
2025-08-19 20:42:29 -0600

    C04 Creacion de Esquemas

---
+++ C02 - M1 - Introducción al Modelo Relacional/20250820 - Segunda Clase - Crear Esquemas.sql
@@ -0,0 +1,14 @@
+-- Crear Esquemas (Schemas)
+-- CREATE SCHEMA NOMBRE
+create schema DBTiendas;
+
+-- create table 
+create table MiPrimera(
+	idtable int,
+	descripcion varchar(100)
+);
+
+-- Qry para consultar todos los registros de una tabla
+select * from miprimera
+
+-- Creen tres tablas para almacenar -- Clientes de un banco --Proveedores -- Direcciones
